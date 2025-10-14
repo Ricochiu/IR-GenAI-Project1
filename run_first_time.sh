73c49e8e35a93f6797eaa302d82991ff30e817f1
Ricochiu <rico10350308@gmail.com>
2025-10-15 01:55:37 +0800

    Update run_first_time.sh

--- run_first_time.sh
+++ run_first_time.sh
@@ -5,6 +5,6 @@ echo "--- 首次執行爬蟲 ---"
 python dual_source_scraper/src/scrape_static.py --pages 5
 
 # 執行動態爬蟲，爬取 5 頁
-python dual_source_scraper/src/scrape_dynamic.py --pages 5
+python dual_source_scraper/src/scrape_dynamic.py --pages 10
 
 echo "--- 首次爬取完成 ---"
